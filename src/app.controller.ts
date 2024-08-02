3453470e012cfab900957647c5aac3cf34da11aa
Fakhri Rossi <rossifakhri@gmail.com>
2024-08-02 22:44:31 +0700

    Routing and Prefix in Controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,13 @@ import { Controller, Get } from "@nestjs/common";
 
 @Controller()
 export class AppController {
-  @Get()
+  @Get("/")
   getRootRoute() {
     return "Hello, World!";
+  }
+
+  @Get("/contact")
+  getContactPage() {
+    return "This is contact!";
   }
 }
